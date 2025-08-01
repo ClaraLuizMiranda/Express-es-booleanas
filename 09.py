12b4b9f09402bf3e5341f8c1a148bf6b1817bbd6
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 21:56:04 -0300

    Create 09.py

---
+++ 09.py
@@ -0,0 +1,3 @@
+Faça uma função que receba 2 números inteiros positivos e calcule o Mínimo Múltiplo Comum (MMC).
+
+Faça uma função que receba 2 números inteiros positivos e calcule o Mínimo Múltiplo Comum (MMC).
