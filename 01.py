ed8d7a28db24b56370b881a70dcbf065524c63b7
ClaraLuizMiranda <mirandamariaclara61@gmail.com>
2025-07-31 21:50:42 -0300

    Create 01.py

---
+++ 01.py
@@ -0,0 +1,8 @@
+Entre com um nome e imprima o nome somente se a primeira letra do nome for “a” (maiúscula ou minúscula).
+
+ p=str(input())
+if p[0]=='a' or p[0]=='A':
+    print(p)
+else:
+    print()
+                                                                               
